ac515aa96db899257ec6a49e5657358c812e1272
WaveARGZ <ajiko0909@gmail.com>
2024-09-15 23:31:06 +0900

    mod

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDJDgJk7ULpc7YH9SlYbzOBT1xkK_rZQvE",
-    authDomain: "line-clone-18b39.firebaseapp.com",
-    projectId: "line-clone-18b39",
-    storageBucket: "line-clone-18b39.appspot.com",
-    messagingSenderId: "762324687470",
-    appId: "1:762324687470:web:a32abfb4510d1cbe66647f"
-})
+        apiKey: "AIzaSyDJDgJk7ULpc7YH9SlYbzOBT1xkK_rZQvE",
+        authDomain: "line-clone-18b39.firebaseapp.com",
+        projectId: "line-clone-18b39",
+        storageBucket: "line-clone-18b39.appspot.com",
+        messagingSenderId: "762324687470",
+        appId: "1:762324687470:web:a32abfb4510d1cbe66647f"
+});
 
 const db = firebaseApp.firestore();
 
